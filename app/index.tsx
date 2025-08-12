2a7708bc5949da3d2fdfcb3e057c75bd1fcde215
Havis Aprinaldi <havisaprinaldi25@gmail.com>
2025-08-12 10:45:38 +0700

    Redesign Slicing Chat ajukan Complaint

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { router } from 'expo-router';
-import { useEffect } from 'react';
-import Splashscreen from '../components/Splashscreen';
+import { router } from "expo-router";
+import { useEffect } from "react";
+import Splashscreen from "../components/Splashscreen";
 
 export default function Index() {
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace('/WelcomePage');
+      router.replace("/WelcomePage");
     }, 2000);
 
     return () => clearTimeout(timer);
