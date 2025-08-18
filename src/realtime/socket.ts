5afc7acda0b185176b7ccffb92bed4e8ff00d0dc
Havis Aprinaldi <havisaprinaldi25@gmail.com>
2025-08-18 20:16:09 +0700

    feat: update api, add modal summary ticket aand make button to clear history chat

--- src/realtime/socket.ts
+++ src/realtime/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
-export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || "http://localhost:4000";
+export const SOCKET_URL =
+  process.env.EXPO_PUBLIC_SOCKET_URL || "http://localhost:4000";
 
 let socket: Socket | null = null;
 
